77cc9e5e8c48e047e58726e691a8afffa434e056
crash33 <35150557+crash33@users.noreply.github.com>
2018-01-05 23:31:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ bot.on('ready', () => {
 
 bot.login(process.env.TOKEN);
 bot.on('message', message => {
-   if ((message.content === ("salope")) || (message.content === ("pute")) || (message.content === ("connard")))
+   if ((message.content === ("salope")) || (message.content === ("pute")) || (message.content === ("connard")) || (message.content === ("salaud")) || (message.content === ("putain")) || (message.content === ("con")))
         message.delete()
     });
