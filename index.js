1f987eefc6680add77cea33be39ed37f45439d65
crash33 <35150557+crash33@users.noreply.github.com>
2018-01-06 14:54:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ bot.on('message', message => {
         message.channel.send('Je vais bien merci :sunglasses: ')
     if ((message.content === ("je t'aime Willi")) || (message.content === ("je t'aime willi")) || (message.content === ("jt'aime willi")) || (message.content === ("je t'aime willi !")) || (message.content === ("je t'aime Willi !")) || (message.content === ("je t'aime Willi !! <3")) || (message.content === ("je t'aime willi !!")) || (message.content === ("je t'aime willi !! <3")) || (message.content === ("Jt'aime willi !!")) || (message.content === ("jt'aime willi <3")) || (message.content === ("jt'aime willi ! <3")) || (message.content === ("je t'aime willi <3")) || (message.content === ("Willi je t'aime <3")) || (message.content === ("willi je t'aime <3")) || (message.content === ("willi jt'aime")) || (message.content === ("Willi jt'aime")) || (message.content === ("Willi jt'aime <3")) || (message.content === ("Willi je t'aime <3")) || (message.content === ("Willi je t'aime")) || (message.content === ("willi je t'aime <3")) || (message.content === ("willi je t'aime")) || (message.content === ("Willi jtm <3")) || (message.content === ("Willi jtm")) || (message.content === ("willi jtm <3")) || (message.content === ("willi jtm")) || (message.content === ("jtm willi")) || (message.content === ("jtm Willi")) || (message.content === ("Jtm willi")) || (message.content === ("Jtm Willi")) || (message.content === ("jtm willi <3")) || (message.content === ("jtm Willi <3")) || (message.content === ("Jtm willi <3")) || (message.content === ("Jtm Willi <3"))){
         random();
+        /*
         if (randnum == 1){
             message.reply('Moi aussi :blush:');
         }
@@ -34,6 +35,9 @@ bot.on('message', message => {
         if (randnum == 6){
             message.reply("Pas moi :laughing:");
         }
+        */
+        var rimage = (rdm[Math.random(0, rdm.Length)]);
+        message.reply(rimage);
     }
     if ((message.content === ("t'as quel age willi?")) || (message.content === ("quel âge as tu willi?")) || (message.content === ("quel âge as-tu willi ?")) || (message.content === ("Quel âge as-tu willi?")) || (message.content === ("t'as quel age Willi?")) || (message.content === ("quel âge as tu Willi ?")) || (message.content === ("Willi t'as quel âge ?")) || (message.content === ("willi t'as quel age?")) || (message.content === ("t'as quel age willi ?")) || (message.content === ("T'as qu'elle age willi?")) || (message.content === ("t'as quel age Willi?")) || (message.content === ("willi quel age as tu ?")) || (message.content === ("willi quel age as tu?")) || (message.content === ("Willi quel age as tu ?")) || (message.content === ("Willi quel age as tu?")) || (message.content === ("Willi quel âge as-tu?")))
         message.channel.send("J'ai 48 ans :older_man:")
