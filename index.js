d956e7792305947ef240711d80345a6159059d2e
crash33 <35150557+crash33@users.noreply.github.com>
2018-01-06 11:44:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -39,8 +39,6 @@ bot.on('message', message => {
         message.channel.send("J'ai 48 ans :older_man:")
     if ((message.content === ("Willi")) || (message.content === ("Willi ?")) || (message.content === ("willi")) || (message.content === ("Willi?")) || (message.contant === ("willi?")))
         message.channel.send("Pour le moment je ne suis qu'un prototype. :robot:")
-    
-    }
 });
 function random(min, max) {
     min = Math.ceil(0);
