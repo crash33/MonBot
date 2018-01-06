8997ccfc50df126ba7c25fe159b536da08b97ff4
crash33 <35150557+crash33@users.noreply.github.com>
2018-01-07 00:18:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -26,6 +26,9 @@ bot.on('message', message => {
         random();
         message.reply(rdm[randnum]);
     }
+    if ((input === ("Es tu une femme ou un homme?")) || (input === ("Es tu une femme ou un homme ?")) || (input === ("Es-tu une femme ou un homme?")) || (input === ("Es tu un homme ou une femme?")) || (input === ("Es tu un homme ou une femme ?")) || (input === ("Es-tu un homme ou une femme ?")) || (input === ("Es-tu un homme ou une femme?")) || (input === ("Es-que t'es une femme ou un homme ?"))){
+        message.reply("Je suis ta mère :sunglasses:", {tts:true});
+    }
 /*=====================[Je t'aime pas willi]=======================*/
     
     
