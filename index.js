71b550f834c5aca7f4fd162a32b442a6ead1c270
crash33 <35150557+crash33@users.noreply.github.com>
2018-01-06 20:19:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 var randnum = 0;
 var prefix = "+";
+var rdm2 = ['Pourquoi dis tu ça?', ':sob:', "C'est méchant ..", "Je ne suis qu'un robot. Je suis gentil ..:sweat_smile:', "Je ne suis qu'un robot qui a ses limites !! :triumph:", 'Ok...'];
 var rdm = ['Arrete... tu va me faire rougir :blush:', 'Moi aussi :heart:', "Désolé de te dire ça mais c'est pas réciproque :sweat_smile:", "Je ne suis qu'une intelligence artificielle.. :sweat_smile: ", ':heart_eyes: ', 'Pegi :underage:'];
 bot.on('ready', () => {
     console.log("Pret");
@@ -14,6 +15,10 @@ bot.on('message', message => {
     if ((input === ("JE T'AIME WILLI")) || (input === ("JE T'AIME WILLI !")) || (input === ("JTM WILLI")) || (input === ("JTM WILLI <3")) || (input === ("I LOVE WILLI")) || (input === ("WILLI JE T'AIME")) || (input === ("WILLI JTM")) || (input === ("JE T AIME WILLI"))){
         random();
         message.reply(rdm[randnum]);
+    }
+    if ((input === ("JE TE DETESTE WILLI")) || (input === ("WILLI TU ES CON")) || (input === ("WILLI T'ES MOCHE")) || (input === ("JE T'AIME PAS WILLI")) || (input === ("WILLI T'ES CON"))){
+        random();
+        message.reply(rdm2[randnum]);
     }
    
 });
