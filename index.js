de579891bb41f198a686fe1f45b6e32372226fa3
crash33 <35150557+crash33@users.noreply.github.com>
2018-01-06 11:42:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,26 +14,27 @@ bot.on('message', message => {
         message.channel.send('Moi aussi :smirk:')
     if ((message.content === ("tu va bien willi?")) || (message.content === ("Tu va bien willi?")) || (message.content === ("Tu va bien Willi ?")) || (message.content === ("tu va bien Willi?")) || (message.content === ("Tu vas bien willi")) || (message.content === ("Tu va bien willi.")) || (message.content === ("Tu vas bien willi..")) || (message.content === ("Tu va bien willi...")) || (message.content === ("tu vas bien willi")) || (message.content === ("tu va bien willi.")) || (message.content === ("tu va bien willi..")) || (message.content === ("tu vas bien willi...")) || (message.content === ("willi comment vas-tu ?")) || (message.content === ("willi comment vas tu?")) || (message.content === ("Willi comment vas-tu?")) || (message.content === ("Willi comment vas-tu ?")) || (message.content === ("willi comment vas-tu ?")))
         message.channel.send('Je vais bien merci :sunglasses: ')
-    if ((message.content === ("je t'aime Willi")) || (message.content === ("je t'aime willi")) || (message.content === ("jt'aime willi")) || (message.content === ("je t'aime willi !")) || (message.content === ("je t'aime Willi !")) || (message.content === ("je t'aime Willi !! <3")) || (message.content === ("je t'aime willi !!")) || (message.content === ("je t'aime willi !! <3")) || (message.content === ("Jt'aime willi !!")) || (message.content === ("jt'aime willi <3")) || (message.content === ("jt'aime willi ! <3")) || (message.content === ("je t'aime willi <3")) || (message.content === ("Willi je t'aime <3")) || (message.content === ("willi je t'aime <3")) || (message.content === ("willi jt'aime")) || (message.content === ("Willi jt'aime")) || (message.content === ("Willi jt'aime <3")) || (message.content === ("Willi je t'aime <3")) || (message.content === ("Willi je t'aime")) || (message.content === ("willi je t'aime <3")) || (message.content === ("willi je t'aime")) || (message.content === ("Willi jtm <3")) || (message.content === ("Willi jtm")) || (message.content === ("willi jtm <3")) || (message.content === ("willi jtm")) || (message.content === ("jtm willi")) || (message.content === ("jtm Willi")) || (message.content === ("Jtm willi")) || (message.content === ("Jtm Willi")) || (message.content === ("jtm willi <3")) || (message.content === ("jtm Willi <3")) || (message.content === ("Jtm willi <3")) || (message.content === ("Jtm Willi <3")))
+    if ((message.content === ("je t'aime Willi")) || (message.content === ("je t'aime willi")) || (message.content === ("jt'aime willi")) || (message.content === ("je t'aime willi !")) || (message.content === ("je t'aime Willi !")) || (message.content === ("je t'aime Willi !! <3")) || (message.content === ("je t'aime willi !!")) || (message.content === ("je t'aime willi !! <3")) || (message.content === ("Jt'aime willi !!")) || (message.content === ("jt'aime willi <3")) || (message.content === ("jt'aime willi ! <3")) || (message.content === ("je t'aime willi <3")) || (message.content === ("Willi je t'aime <3")) || (message.content === ("willi je t'aime <3")) || (message.content === ("willi jt'aime")) || (message.content === ("Willi jt'aime")) || (message.content === ("Willi jt'aime <3")) || (message.content === ("Willi je t'aime <3")) || (message.content === ("Willi je t'aime")) || (message.content === ("willi je t'aime <3")) || (message.content === ("willi je t'aime")) || (message.content === ("Willi jtm <3")) || (message.content === ("Willi jtm")) || (message.content === ("willi jtm <3")) || (message.content === ("willi jtm")) || (message.content === ("jtm willi")) || (message.content === ("jtm Willi")) || (message.content === ("Jtm willi")) || (message.content === ("Jtm Willi")) || (message.content === ("jtm willi <3")) || (message.content === ("jtm Willi <3")) || (message.content === ("Jtm willi <3")) || (message.content === ("Jtm Willi <3"))){
         random();
         if (randnum == 1){
             message.reply('Moi aussi :blush:');
         }
-        if (randum == 2){
+        if (randnum == 2){
             message.reply(':blush:');
         }
-        if (randum == 3){
+        if (randnum == 3){
             message.reply('Arrete... tu va me faire rougir :kissing_closed_eyes:');
         }
-        if (randum == 4){
+        if (randunm == 4){
             message.reply("Ce n'est pas réciproque :laughing:");
         }
-        if (randum == 5){
+        if (randnum == 5){
             message.reply("Pas moi <3");
         }
-        if (randum == 6){
+        if (randnum == 6){
             message.reply("Pas moi :laughing:");
         }
+    }
     if ((message.content === ("t'as qu'elle age willi?")) || (message.content === ("quel âge as tu willi?")) || (message.content === ("qu'elle âge as-tu willi ?")) || (message.content === ("Qu'elle âge as-tu willi?")) || (message.content === ("t'as qu'elle age Willi?")) || (message.content === ("quel âge as tu Willi ?")) || (message.content === ("Willi t'as qu'elle âge ?")) || (message.content === ("willi t'as qu'elle age?")) || (message.content === ("t'as qu'elle age willi ?")) || (message.content === ("T'as qu'elle age willi?")) || (message.content === ("t'as qu'elle age Willi?")) || (message.content === ("willi qu'elle age as tu ?")) || (message.content === ("willi qu'elle age as tu?")) || (message.content === ("Willi qu'elle age as tu ?")) || (message.content === ("Willi qu'elle age as tu?")) || (message.content === ("Willi qu'elle âge as-tu?")))
         message.channel.send("J'ai 48 ans :older_man:")
     if ((message.content === ("Willi")) || (message.content === ("Willi ?")) || (message.content === ("willi")) || (message.content === ("Willi?")) || (message.contant === ("willi?")))
