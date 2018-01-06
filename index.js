dd9a73b44f6cac1e53b47d518f6b24df81145a2b
crash33 <35150557+crash33@users.noreply.github.com>
2018-01-06 17:25:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bot = new Discord.Client();
 var prefix = "+";
 var randnum = 0;
 var rdm = ['Arrete... tu va me faire rougir', 'Je te veux dans mon lit !', 'OUI', 'Suce moi !', 'test4', 'test5'];
+var msg = message.content.toUpperCase();
 bot.on('ready', () => {
     console.log("Pret");
 });
@@ -49,7 +50,7 @@ bot.on('message', message => {
         message.channel.send(coucou);
         message.channel.send("Message supprimé");
     }
-    if (message.includes("quenouille")) {
+    if (msg.includes("quenouille")) {
         message.delete();
         message.author.send("coucou");
     }
