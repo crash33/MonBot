6d2df6d80946d8cf329b452cb97f0d857662a133
crash33 <35150557+crash33@users.noreply.github.com>
2018-01-07 15:01:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var prefix = "+";
 /*===================[ Variable / message ] ====================*/
 
 var rdm2 = ['Pourquoi dis tu ça?', ':sob:', "C'est méchant ..", "Je ne suis qu'un robot. Je suis gentil ..:sweat_smile:", 'Je suis un robot certe mais il y a des limites :triumph:', 'Ok...'];
-var rdm = ['Arrete... tu va me faire rougir :blush:', 'Moi aussi :heart:', "Désolé de te dire ça mais c'est pas réciproque :stuck_out_tongue_closed_eyes: http://www.rethinkrobotics.com/wp-content/uploads/2017/03/Dab-301x150-300x150.jpg", "Je ne suis qu'une intelligence artificielle.. :sweat_smile: ", ':heart_eyes: ', 'Pegi :underage:'];
+var rdm = ['Arrete... tu va me faire rougir :blush:', 'Moi aussi :heart:', "Désolé de te dire ça mais c'est pas réciproque :stuck_out_tongue_closed_eyes:", "Je ne suis qu'une intelligence artificielle.. :sweat_smile: ", ':heart_eyes: ', 'Pegi :underage:'];
 
 /*==========================[ Start ]============================*/
 
