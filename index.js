c78e98486f683904b91adcbcae03e706732ab18a
crash33 <35150557+crash33@users.noreply.github.com>
2018-01-10 16:26:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ bot.on('message', message => {
     if (message.member.roles.some(r=>["role_name"].includes(r.name)) ) {
         if(message.content.startsWith(prefix + "clear")) {
             const deleteCount = parseInt(args[0], 10);
-            if(!deleteCount || deleteCount < 2 || deleteCount > 100)
-            return message.reply("Il faut mettre un chiffre entre 1 et 100...");
+            if(!deleteCount || deleteCount < 2 || deleteCount > 100){
+                return message.reply("Il faut mettre un chiffre entre 1 et 100...");
+            }
             const fetched = await message.channel.fetchMessages({count: deleteCount});
             message.channel.bulkDelete(fetched)
-            .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
         }
     }
 });
