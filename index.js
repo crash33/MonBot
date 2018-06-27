ac4d16d142a2f255d5afaef940831e87a7a16c5c
crash33 <35150557+crash33@users.noreply.github.com>
2018-06-27 15:37:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,5 +6,12 @@ bot.on('ready', () => {
     console.log("Pret");
     bot.user.setPresence({ game: { name: "Modérer"}});
 });
-
+bot.on('message', message => {
+    if (message.content === "info.use.less"){
+        var embed = new Discord.RichEmbed()
+        .setTitle("Informations")
+        .setDescription("Pour se connecter à différents salons, veuillez vous rendre sur #authentification")
+        message.channel.sendEmbed(embed);
+    }
+});
 
