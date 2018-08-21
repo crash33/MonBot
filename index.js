8d4aedb329d09a25dbf0b9260a54276c12d9d206
crash33 <35150557+crash33@users.noreply.github.com>
2018-08-22 00:01:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ bot.on('message', message => {
             .addField("Réglement :","Il est interdit d'insulter mais aussi de faire de la publicité ou d'avoir un pseudo inapproprié. ", false)
             .setColor("0x0053ce")
             .setFooter("Family")
-            .setThumbnail("http://zupimages.net/viewer.php?id=18/34/f6xe.png")
+            .setThumbnail("https://zupimages.net/up/18/34/f6xe.png")
             message.channel.sendEmbed(embed);
         }
         if (message.content === "sondage"){
             var embedsond = new Discord.RichEmbed()
-            .setAuthor("staff", "http://zupimages.net/viewer.php?id=18/34/f6xe.png")
+            .setAuthor("staff", "https://zupimages.net/up/18/34/f6xe.png")
             .setTitle("Sondage")
             .setDescription("Aimez-vous le serveur ?")
             .setColor("0xB40404")
