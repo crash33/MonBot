dfa8d677c944c92eff5244907cc35209fc46476b
crash33 <35150557+crash33@users.noreply.github.com>
2018-08-22 14:05:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ bot.on('message', message => {
         }
         if (message.content === "choixxxpeau"){
             var embedss = new Discord.RichEmbed()
-            .setAuthor("staff", "https://zupimages.net/up/18/34/f6xe.png")
+            .setAuthor("Poudlard", "https://zupimages.net/up/18/34/f6xe.png")
             .setTitle("Aide :")
             .setDescription("Le choixpeau ne sait pas dans quelle maison te mettre, aide le en utilisant le sort suivante : HP-gryffondor ,HP-serpentard ,HP-poufsouffle ,HP-serdaigle")
             .setColor("0x0E8E7D")
